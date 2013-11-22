6523e0f8695b6ba1860d1a6df866a50a67021a1e
gsh199449 <gsh199449@hotmail.com>
2013-11-22 23:12:13 +0800

    Json解析出现错误 总是多空格

--- src/com/gs/indexer/ContentReader.java
+++ src/com/gs/indexer/ContentReader.java
@@ -8,6 +8,7 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.nio.CharBuffer;
+import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
 
@@ -23,16 +24,67 @@ import com.gs.crawler.PagePOJO;
  * @packageName com.gs.io
  */
 public class ContentReader {
-	private Logger logger = Logger.getLogger(this.getClass());
-
-	public List<PagePOJO> read(String path) throws IOException {
-		LinkedList<String> list = (LinkedList<String>) FileUtils.readLines(new File(path));
+	private FileInputStream fis;
+	private long flag = 0;
+	public static LinkedList<PagePOJO> readFileToPOJOs(String path) throws IOException {
+		List<String> list = FileUtils.readLines(new File(path));
 		Gson g = new Gson();
 		LinkedList<PagePOJO> re = new LinkedList<PagePOJO>();
-		while (!list.isEmpty()) {
-			re.add(g.fromJson(list.remove(), PagePOJO.class));
+		Iterator<String> it = list.iterator();
+		while (it.hasNext()) {
+			re.add(g.fromJson(it.next(), PagePOJO.class));
 		}
 		return re;
 	}
+	
+	public static PagePOJO read(FileInputStream fis, long startoffset) {
+		String json = null;
+		try {
+			fis.skip(startoffset);
+			byte[] b1 = new byte[99999];// Buffer
+			byte b;
+			for (int i = 0; (b = (byte) fis.read()) != -1 && b != '}'; i++) {
+				b1[i] = b;
+			}
+			json = new String(b1)+'}';
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		Gson gson = new Gson();
+		return gson.fromJson(json, PagePOJO.class);
+	}
+	
+	public ContentReader(FileInputStream fis){
+		this.fis = fis;
+	}
+	public Hit next(){
+		Hit hit = new Hit();
+		String json = null;
+		int i = 0;
+		try {
+			fis.skip(flag);
+			byte[] b1 = new byte[99999];// Buffer
+			byte b;
+			for (i = 0; (b = (byte) fis.read()) != -1 && b != '}'; i++) {
+				b1[i] = b;
+			}
+			b1[i] = '}';
+			json = new String(b1);
+			System.out.println(json);
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		Gson gson = new Gson();
+		PagePOJO pojo = gson.fromJson(json, PagePOJO.class);//FIXME
+		hit.setPagePOJO(pojo);
+		hit.setStartOffset(flag);
+		flag = flag + i;
+		return hit;
+	}
+
 
 }
--- test/com/gs/test/TestContentReader.java
+++ test/com/gs/test/TestContentReader.java
@@ -2,30 +2,33 @@ package com.gs.test;
 
 import static org.junit.Assert.*;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.LinkedList;
 
 import org.junit.Test;
 
+import com.google.gson.Gson;
 import com.gs.crawler.PagePOJO;
 import com.gs.indexer.ContentReader;
+import com.gs.indexer.Hit;
 
 public class TestContentReader {
 
 	@Test
-	public void test() {
-		ContentReader cr = new ContentReader();
-		try {
-			LinkedList<PagePOJO> l = cr.readFileToPOJOs("D:\\Workspaces\\GitHubRepository\\DistributeCrawler\\NewsJson\\20131116");
-			int i=0;
-			for(PagePOJO pojo : l){
-				System.out.println(pojo);
-				i++;
-				if(i > 5)break;
-			}
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
+	public void test() throws FileNotFoundException {
+		ContentReader cr = new ContentReader(new FileInputStream(new File("D://Test//testByte.txt")));
+		cr.next();
+		
+	}
+	
+	@Test
+	public void testJson(){
+		Gson g = new Gson();
+		String d = g.toJson(new PagePOJO("rrr",2,"yyy","t"));
+		System.out.println(d);
 	}
 
 }
