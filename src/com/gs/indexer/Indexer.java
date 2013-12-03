a9480ce9669cc90512899bfcdc7d2f2e999cfe07
gsh199449 <gsh199449@hotmail.com>
2013-12-03 22:52:19 +0800

    添加了indexForCarrot2

--- src/com/gs/indexer/Indexer.java
+++ src/com/gs/indexer/Indexer.java
@@ -7,6 +7,7 @@ import org.apache.log4j.Logger;
 import org.apache.lucene.document.Document;
 import org.apache.lucene.document.Field;
 import org.apache.lucene.document.NumericField;
+import org.apache.lucene.document.Field.Store;
 import org.apache.lucene.index.CorruptIndexException;
 import org.apache.lucene.index.IndexWriter;
 import org.apache.lucene.index.IndexWriterConfig;
@@ -92,6 +93,43 @@ public class Indexer {
 					doc.add(new NumericField("startOffset", Field.Store.YES, false).setLongValue(hit.getStartOffset()));
 					doc.add(new Field("filename", hit.getFileName(),
 							Field.Store.YES, Field.Index.NOT_ANALYZED));
+					writer.addDocument(doc);
+				}
+				jr.close();
+			}
+			writer.close();
+			
+		} catch (CorruptIndexException e) {
+			logger.error(e.getMessage());
+		} catch (LockObtainFailedException e) {
+			logger.error(e.getMessage());
+		} catch (IOException e) {
+			logger.error(e.getMessage());
+		} catch (Exception e) {
+			logger.error(e.getMessage());
+		}
+	}
+	
+	public void indexForCarrot2(String indexField, String docsField) {
+		try {
+			Directory directory = FSDirectory.open(new File(indexField));
+			IndexWriterConfig conf = new IndexWriterConfig(Version.LUCENE_35,
+					new IKAnalyzer());
+			IndexWriter writer = new IndexWriter(directory, conf);
+			File f = new File(docsField);
+			Document doc;
+			for (File file : f.listFiles()) {
+				logger.info("Indexing   " + file.getName());
+				JsonReader jr = new JsonReader(file);
+				while (jr.hasNext()) {
+					doc = new Document();
+					Hit hit = jr.next();
+					if(hit == null) continue;
+					 doc.add(new Field("fullContent", hit.getPagePOJO().content, Field.Store.NO,Field.Index.ANALYZED));
+
+			            doc.add(new Field("title", hit.getPagePOJO().getTitle(), Field.Store.YES,Field.Index.ANALYZED));
+			            doc.add(new Field("snippet", hit.getPagePOJO().content, Field.Store.YES,Field.Index.ANALYZED));
+			            doc.add(new Field("url", hit.getPagePOJO().url, Field.Store.YES,Field.Index.ANALYZED));
 					writer.addDocument(doc);
 				}
 				jr.close();
