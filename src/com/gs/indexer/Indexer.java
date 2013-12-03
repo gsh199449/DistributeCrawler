7e707ab90fc7d3713bce4363845e0d85cd19d62c
gsh199449 <gsh199449@hotmail.com>
2013-12-03 20:18:02 +0800

    增加了制定数量的搜索方法。

--- src/com/gs/indexer/Indexer.java
+++ src/com/gs/indexer/Indexer.java
@@ -65,4 +65,47 @@ public class Indexer {
 			logger.error(e.getMessage());
 		}
 	}
+	
+	/**
+	 * @author GS
+	 * @param indexField
+	 * @param docsField
+	 * @param docnumber 需要的结果数量
+	 */
+	public void index(String indexField, String docsField,int docnumber) {
+		try {
+			Directory directory = FSDirectory.open(new File(indexField));
+			IndexWriterConfig conf = new IndexWriterConfig(Version.LUCENE_35,
+					new IKAnalyzer());
+			IndexWriter writer = new IndexWriter(directory, conf);
+			File f = new File(docsField);
+			Document doc;
+			for (File file : f.listFiles()) {
+				logger.info("Indexing   " + file.getName());
+				JsonReader jr = new JsonReader(file);
+				while (jr.hasNext()) {
+					doc = new Document();
+					Hit hit = jr.next();
+					if(hit == null) continue;
+					doc.add(new Field("content", hit.getPagePOJO().getContent(), Field.Store.NO,
+							Field.Index.ANALYZED));
+					doc.add(new NumericField("startOffset", Field.Store.YES, false).setLongValue(hit.getStartOffset()));
+					doc.add(new Field("filename", hit.getFileName(),
+							Field.Store.YES, Field.Index.NOT_ANALYZED));
+					writer.addDocument(doc);
+				}
+				jr.close();
+			}
+			writer.close();
+			
+		} catch (CorruptIndexException e) {
+			logger.error(e.getMessage());
+		} catch (LockObtainFailedException e) {
+			logger.error(e.getMessage());
+		} catch (IOException e) {
+			logger.error(e.getMessage());
+		} catch (Exception e) {
+			logger.error(e.getMessage());
+		}
+	}
 }
