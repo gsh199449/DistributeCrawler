c315ede0f0a9f057c4b5fc62d65e2d82faa04635
gsh199449 <gsh199449@hotmail.com>
2013-12-20 20:55:04 +0800

    修改了一些注释

--- src/com/gs/Classifier/StopWordsHandler.java
+++ src/com/gs/Classifier/StopWordsHandler.java
@@ -8,20 +8,13 @@ import java.util.Set;
 import org.apache.commons.io.FileUtils;
 
 /**
-* 停用词处理器
-* @author phinecos 
+* @author GS 
 * 
 */
 public class StopWordsHandler 
 {
-	//private static String stopWordsList[] ={"的", "我们","要","自己","之","将","“","”","，","（","）","后","应","到","某","后","个","是","位","新","一","两","在","中","或","有","更","好",""};//常用停用词
 	public static boolean IsStopWord(String word)
 	{
-		/*for(int i=0;i<stopWordsList.length;++i)
-		{
-			if(word.equalsIgnoreCase(stopWordsList[i]))
-				return true;
-		}*/
 		Set<String> set = new HashSet<String>();
 		try {
 			for (String e : FileUtils.readLines(new File("D://Test//ChineseStopwords.txt"))) {
